5303cd8b1fce53b0bdb90a60d6ab1b976339a1ac
Christian <christiancazu@gmail.com>
2020-05-21 18:08:51 -0500

    setup config module

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,14 @@ module.exports = {
     jest: true,
   },
   rules: {
+    'no-trailing-spaces': 'error',
+    'eol-last': ["error", "always"],
+    indent: ["error", 2, { SwitchCase: 1 }],
+    quotes: ["error", "single", { "allowTemplateLiterals": true } ],
+    semi: ["error", "always"],
+    "linebreak-style": 0,
+    "no-empty": ["error", { "allowEmptyCatch": true }],
+    // ts
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = parseInt(process.env.PORT, 10) || 3000;
+  await app.listen(port);
+
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(`running on: http://localhost:${port}`);
+  }
 }
 bootstrap();
