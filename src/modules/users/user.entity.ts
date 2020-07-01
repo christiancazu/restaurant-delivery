faa859fa66029622a44b8424328336d776376b2c
Maicol <maicolcrodrigoa@gmail.com>
2020-07-01 15:44:31 -0500

    added user entity

--- src/modules/users/user.entity.ts
+++ src/modules/users/user.entity.ts
@@ -13,23 +13,97 @@ import { Role } from '../roles/role.entity';
 
 @Entity('users')
 export class User extends BaseEntity {
-
   @PrimaryGeneratedColumn('increment')
   id: number;
 
   @Column({
     type: 'varchar',
     unique: true,
-    length: 25
+    length: 16,
+    nullable: false,
+  })
+  document: string;
+
+  @Column({
+    type: 'varchar',
+    length: 32,
+    nullable: false,
+  })
+  firstname: string;
+
+  @Column({
+    type: 'varchar',
+    length: 32,
+    nullable: false,
+  })
+  lastname: string;
+
+  @Column({
+    type: 'varchar',
+    length: 32,
+    unique: true,
+    nullable: false,
+  })
+  username: string;
+
+  @Column({
+    type: 'varchar',
+    length: 32,
+    nullable: false,
+  })
+  password: string;
+
+  @Column({
+    type: 'varchar',
+    length: 64,
+    nullable: true,
+  })
+  adress: string;
+
+  @Column({
+    type: 'varchar',
+    length: 16,
+    unique: true,
+    nullable: false,
+  })
+  phone: string;
+
+  @Column({
+    type: 'varchar',
+    unique: true,
+    length: 64,
+    nullable: false,
   })
   email: string;
 
-  @Column({ type: 'varchar' })
-  password: string;
+  @Column({
+    type: 'varchar',
+    length: 128,
+    nullable: true,
+  })
+  locationLatLng: string;
+
+  @Column({
+    type: 'varchar',
+    length: 32,
+    nullable: true,
+  })
+  avatar: string;
+
+  @Column({
+    type: 'boolean',
+    nullable: false,
+  })
+  enabled: boolean;
 
   @ManyToMany(() => Role, { cascade: true })
-  @JoinTable({ name: 'users_roles', })
+  @JoinTable({ name: 'users_roles' })
   roles: Role[];
+
+  @BeforeInsert()
+  beforeInsertActions() {
+    this.enabled = true;
+  }
 
   @BeforeInsert()
   async hashPassword(): Promise<void> {
